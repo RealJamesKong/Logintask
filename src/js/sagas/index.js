0388ffc1207af4a9e3800afce032f34ab2b24835
RealJamesKong <jk5217@nyu.edu>
2017-05-28 14:27:07 -0400

    Update index.js

--- src/js/sagas/index.js
+++ src/js/sagas/index.js
@@ -3,8 +3,8 @@ import axios from "axios";
 
 export default function* checkLogin(action){
   try {
-    const user = yield call(axios.post("http://api.edu.chat/v1/api/login/",
-      {username: action.payload.id, password:action.payload.pw, platform:"web"}));
+    const user = yield call(axios.post, "http://api.edu.chat/v1/api/login/",
+        action.payload.id, action.payload.pw, "web");
       yield put({type: "LOGIN_SUCCESS"});
     } catch(e){
       yield put({type: "LOGIN_FAIL"});
