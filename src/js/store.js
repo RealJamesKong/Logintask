f8bfa04334ba3634a47db0cf315dd415f8132991
RealJamesKong <jk5217@nyu.edu>
2017-06-04 02:02:45 -0400

    Update store.js

--- src/js/store.js
+++ src/js/store.js
@@ -1,15 +1,17 @@
 import { applyMiddleware, createStore } from "redux"
-import createSagaMiddleware from 'redux-saga'
+//import createSagaMiddleware from 'redux-saga'
 
+import logger from "redux-logger"
+import thunk from "redux-thunk"
 import promise from "redux-promise-middleware"
-import mySaga from './sagas'
+//import mySaga from './sagas'
 
 import reducer from "./reducers"
 
-const sagaMiddleware = createSagaMiddleware()
+//const sagaMiddleware = createSagaMiddleware()
 
-const middleware = applyMiddleware( sagaMiddleware,promise())
+const middleware = applyMiddleware( promise(), thunk, logger())
 
 export default createStore(reducer, middleware)
 
-sagaMiddleware.run(mySaga)
+//sagaMiddleware.run(mySaga)
