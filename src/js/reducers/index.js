c698827301752763571ce9a407cc87034bd6f519
RealJamesKong <jk5217@nyu.edu>
2017-06-04 23:00:11 -0400

    Update index.js

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,9 +1,9 @@
 export default function reducer(state={success: null, username: ""}, action) {
   switch (action.type) {
-    case "LOGIN_SUCCESS": {
+    case "LOGIN_SUCCESSFUL": {
       return {...state,success:true, username:action.payload}
     }
-    case "LOGIN_FAIL": {
+    case "LOGIN_FAILED": {
       return {...state,success:false}
     }
   }
