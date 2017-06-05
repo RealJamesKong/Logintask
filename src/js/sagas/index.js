6419276caa586e3a4c4fe923718c26acb7e6ffb7
RealJamesKong <jk5217@nyu.edu>
2017-06-04 23:00:48 -0400

    Update index.js

--- src/js/sagas/index.js
+++ src/js/sagas/index.js
@@ -1,16 +1,27 @@
 import { call,put, takeEvery, takeLatest } from 'redux-saga/effects'
 import axios from "axios";
 
-export default function* checkLogin(action){
-  try {
-    const user = yield call(axios.post, "http://api.edu.chat/v1/api/login/",
-        action.payload.id, action.payload.pw, "web");
-      yield put({type: "LOGIN_SUCCESS"});
-    } catch(e){
-      yield put({type: "LOGIN_FAIL"});
+
+function sendRequest(id, pw) {
+    return axios.post("http://api.edu.chat/v1/api/login/",
+        {username: id, password: pw, platform: "web"});
+}
+
+
+function* checkLogin(action) {
+    if (action != undefined) {
+        try {
+            const user = yield call(sendRequest, action.payload.id, action.payload.pw);
+            yield put({type: "LOGIN_SUCCESSFUL", payload: action.payload.id});
+        } catch (e) {
+            yield put({type: "LOGIN_FAILED"});
+        }
     }
 }
 
+
 function* mySaga(){
-  yield takeEvery("LOGIN_SUBMITTED", checkLogin);
+    yield takeEvery("LOGIN_SUBMITTED", checkLogin);
 }
+
+export default mySaga;
